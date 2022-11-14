2e1ff41a3c7cfc83b253e0692cf5d7a50e5bf71d
applebaumlowegold <118147703+applebaumlowegold@users.noreply.github.com>
2022-11-13 23:05:37 -0500

    Add files via upload

---
+++ Watchesjs.js
@@ -0,0 +1,18 @@
+// feather.replace()
+const nowYr = new Date();
+document.getElementById("jsyear").innerHTML = nowYr.getFullYear();
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active');
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
